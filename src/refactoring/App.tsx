3a1870efb6c8cd2262fc28c782e9b86a5fcdd1cb
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-20 18:12:27 +0900

    useProducts 수정

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -4,33 +4,6 @@ import { AdminPage, CartPage } from "./components";
 import { Coupon, Product } from "../types.ts";
 import { useCoupons, useProducts } from "./hooks";
 
-const initialProducts: Product[] = [
-	{
-		id: "p1",
-		name: "상품1",
-		price: 10000,
-		stock: 20,
-		discounts: [
-			{ quantity: 10, rate: 0.1 },
-			{ quantity: 20, rate: 0.2 },
-		],
-	},
-	{
-		id: "p2",
-		name: "상품2",
-		price: 20000,
-		stock: 20,
-		discounts: [{ quantity: 10, rate: 0.15 }],
-	},
-	{
-		id: "p3",
-		name: "상품3",
-		price: 30000,
-		stock: 20,
-		discounts: [{ quantity: 10, rate: 0.2 }],
-	},
-];
-
 const initialCoupons: Coupon[] = [
 	{
 		name: "5000원 할인 쿠폰",
@@ -47,7 +20,7 @@ const initialCoupons: Coupon[] = [
 ];
 
 const App = () => {
-	const { products, updateProduct, addProduct } = useProducts(initialProducts);
+	const { products, updateProduct, addProduct } = useProducts();
 	const { coupons, addCoupon } = useCoupons(initialCoupons);
 	const [isAdmin, setIsAdmin] = useState(false);
 
--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,52 @@
-import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { useState } from "react";
 
-export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+import { Product } from "../../types.ts";
+
+export const useProducts = () => {
+	const [products, setProducts] = useState<Product[]>(initialProducts);
+
+	function addProduct(newProduct: Product) {
+		setProducts((prevProducts) => [...prevProducts, newProduct]);
+	}
+
+	function updateProduct(updatedProduct: Product) {
+		setProducts((prevProducts) =>
+			prevProducts.map((curProduct) =>
+				curProduct.id === updatedProduct.id ? updatedProduct : curProduct
+			)
+		);
+	}
+
+	return {
+		products,
+		addProduct,
+		updateProduct,
+	};
 };
+
+const initialProducts: Product[] = [
+	{
+		id: "p1",
+		name: "상품1",
+		price: 10000,
+		stock: 20,
+		discounts: [
+			{ quantity: 10, rate: 0.1 },
+			{ quantity: 20, rate: 0.2 },
+		],
+	},
+	{
+		id: "p2",
+		name: "상품2",
+		price: 20000,
+		stock: 20,
+		discounts: [{ quantity: 10, rate: 0.15 }],
+	},
+	{
+		id: "p3",
+		name: "상품3",
+		price: 30000,
+		stock: 20,
+		discounts: [{ quantity: 10, rate: 0.2 }],
+	},
+];
