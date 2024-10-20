31ef2114ef30a557415ca489b709f50aecc26c69
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-20 18:12:27 +0900

    useProducts 수정

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -3,6 +3,33 @@ import { AdminPage, CartPage } from "./components";
 
 import { useCoupons, useProducts } from "./hooks";
 
+const initialProducts: Product[] = [
+	{
+		id: "p1",
+		name: "상품1",
+		price: 10000,
+		stock: 20,
+		discounts: [
+			{ quantity: 10, rate: 0.1 },
+			{ quantity: 20, rate: 0.2 },
+		],
+	},
+	{
+		id: "p2",
+		name: "상품2",
+		price: 20000,
+		stock: 20,
+		discounts: [{ quantity: 10, rate: 0.15 }],
+	},
+	{
+		id: "p3",
+		name: "상품3",
+		price: 30000,
+		stock: 20,
+		discounts: [{ quantity: 10, rate: 0.2 }],
+	},
+];
+
 const initialCoupons: Coupon[] = [
 	{
 		name: "5000원 할인 쿠폰",
@@ -19,7 +46,7 @@ const initialCoupons: Coupon[] = [
 ];
 
 const App = () => {
-	const { products, updateProduct, addProduct } = useProducts();
+	const { products, updateProduct, addProduct } = useProducts(initialProducts);
 	const { coupons, addCoupon } = useCoupons(initialCoupons);
 	const [isAdmin, setIsAdmin] = useState(false);
 
