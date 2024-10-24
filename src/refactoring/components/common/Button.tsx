612c4d3c4cba92a495627d7baf17cf82d10966cd
taeyoung <terry.hwang@h2ohospitality.io>
2024-10-24 16:04:00 +0900

    button styleType 추가

--- src/refactoring/components/common/Button.tsx
+++ src/refactoring/components/common/Button.tsx
@@ -1,10 +1,25 @@
 import { joinClassName } from "../../utils";
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+	styleType?: "blue" | "red" | "green";
+};
 
-export function Button({ className, children, ...rest }: Props) {
+export function Button({ className, children, styleType, ...rest }: Props) {
+	const styleTypeClassName = {
+		none: undefined,
+		blue: "bg-blue-500 text-white hover:bg-blue-600",
+		red: "bg-red-500 text-white hover:bg-red-600",
+		green: "bg-green-500 text-white hover:bg-green-600",
+	};
 	return (
-		<button className={joinClassName("px-4 py-2 rounded", className)} {...rest}>
+		<button
+			className={joinClassName(
+				"px-4 py-2 rounded",
+				className,
+				styleTypeClassName[styleType ?? "none"]
+			)}
+			{...rest}
+		>
 			{children}
 		</button>
 	);
