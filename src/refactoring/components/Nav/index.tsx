ca60b2806d803764aa74bc54d1eeda95bf9bf1a6
taeyoung <terry.hwang@h2ohospitality.io>
2024-10-24 15:53:21 +0900

    nav 공통 컴포넌트로 변경

--- src/refactoring/components/Nav/index.tsx
+++ src/refactoring/components/Nav/index.tsx
@@ -1,3 +1,5 @@
+import { Button, Container, PageTitle } from "../common";
+
 type Props = {
 	isAdmin: boolean;
 	handleClickToggleAdminButton: () => void;
@@ -6,15 +8,15 @@ type Props = {
 export function Nav({ isAdmin, handleClickToggleAdminButton }: Props) {
 	return (
 		<nav className="bg-blue-600 text-white p-4">
-			<div className="container mx-auto flex justify-between items-center">
-				<h1 className="text-2xl font-bold">쇼핑몰 관리 시스템</h1>
-				<button
+			<Container className="container mx-auto flex justify-between items-center">
+				<PageTitle>쇼핑몰 관리 시스템</PageTitle>
+				<Button
 					onClick={handleClickToggleAdminButton}
-					className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-blue-100"
+					className="bg-white text-blue-600  hover:bg-blue-100"
 				>
 					{isAdmin ? "장바구니 페이지로" : "관리자 페이지로"}
-				</button>
-			</div>
+				</Button>
+			</Container>
 		</nav>
 	);
 }
