d642e5d81828c26827261d5df11393784edd0fb2
terry.hwang <terry.hwang@h2ohospitality.io>
2024-10-25 01:22:48 +0900

    util로 분리

---
+++ src/refactoring/hooks/Cart/utils/index.ts
@@ -0,0 +1,14 @@
+export const getAppliedDiscount = (cartItem: CartItem) => {
+	const { product, quantity } = cartItem;
+	const { discounts } = product;
+
+	const appliedDiscount = discounts.filter(
+		({ quantity: discountQuantity }) => quantity >= discountQuantity
+	);
+
+	if (appliedDiscount.length === 0) {
+		return 0;
+	}
+
+	return Math.max(...appliedDiscount.map(({ rate }) => rate));
+};
