6c8440b733e450bc7c56f1e483dba269b07aa551
taeyoung <terry.hwang@h2ohospitality.io>
2024-10-24 15:58:03 +0900

    App 컴포넌트 정리

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,56 +1,10 @@
 import { useState } from "react";
 
 import { Nav, AdminPage, CartPage } from "./components";
-
 import { CombinedContextProvider } from "./context/combinedContext";
-import { useCoupons, useProducts } from "./hooks";
 import { RootLayout, Layout } from "./components/layouts";
 
-const initialProducts: Product[] = [
-	{
-		id: "p1",
-		name: "상품1",
-		price: 10000,
-		stock: 20,
-		discounts: [
-			{ quantity: 10, rate: 0.1 },
-			{ quantity: 20, rate: 0.2 },
-		],
-	},
-	{
-		id: "p2",
-		name: "상품2",
-		price: 20000,
-		stock: 20,
-		discounts: [{ quantity: 10, rate: 0.15 }],
-	},
-	{
-		id: "p3",
-		name: "상품3",
-		price: 30000,
-		stock: 20,
-		discounts: [{ quantity: 10, rate: 0.2 }],
-	},
-];
-
-const initialCoupons: Coupon[] = [
-	{
-		name: "5000원 할인 쿠폰",
-		code: "AMOUNT5000",
-		discountType: "amount",
-		discountValue: 5000,
-	},
-	{
-		name: "10% 할인 쿠폰",
-		code: "PERCENT10",
-		discountType: "percentage",
-		discountValue: 10,
-	},
-];
-
 const App = () => {
-	const { products, updateProduct, addProduct } = useProducts(initialProducts);
-	const { coupons, addCoupon } = useCoupons(initialCoupons);
 	const [isAdmin, setIsAdmin] = useState(!false);
 
 	const handleClickToggleAdminButton = () => {
@@ -64,19 +18,7 @@ const App = () => {
 					isAdmin={isAdmin}
 					handleClickToggleAdminButton={handleClickToggleAdminButton}
 				/>
-				<Layout>
-					{isAdmin ? (
-						<AdminPage
-							products={products}
-							coupons={coupons}
-							onProductUpdate={updateProduct}
-							onProductAdd={addProduct}
-							onCouponAdd={addCoupon}
-						/>
-					) : (
-						<CartPage products={products} coupons={coupons} />
-					)}
-				</Layout>
+				<Layout>{isAdmin ? <AdminPage /> : <CartPage />}</Layout>
 			</RootLayout>
 		</CombinedContextProvider>
 	);
